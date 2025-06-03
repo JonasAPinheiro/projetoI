90b984ea28511b313dc3e4afa188f2c851b66209
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-03 17:43:50 -0300

    Criação do repository 'CategoriaUsuarioRepository'

---
+++ dist/repository/CategoriaUsuarioRepository.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categoriasUsuario = void 0;
+const CategoriaUsuarioEntity_1 = require("../model/CategoriaUsuarioEntity");
+exports.categoriasUsuario = [
+    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(undefined, "Professor"),
+    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(undefined, "Aluno"),
+    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(undefined, "Bibliotecário")
+];
---
+++ src/repository/CategoriaUsuarioRepository.ts
@@ -0,0 +1,7 @@
+import { CategoriaUsuarioEntity } from "../model/CategoriaUsuarioEntity";
+
+export const categoriasUsuario: CategoriaUsuarioEntity[] = [
+  new CategoriaUsuarioEntity(undefined, "Professor"),
+  new CategoriaUsuarioEntity(undefined, "Aluno"),
+  new CategoriaUsuarioEntity(undefined, "Bibliotecário")
+];
