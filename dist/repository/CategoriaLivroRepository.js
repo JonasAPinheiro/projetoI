dbdc0b225a16a349e3891ccbc6f0cbd9ea352aab
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-03 17:50:54 -0300

    Criação do repository 'CategoriaLivroRepository'

---
+++ dist/repository/CategoriaLivroRepository.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categoriasLivro = void 0;
+const CategoriaLivroEntity_1 = require("../model/CategoriaLivroEntity");
+exports.categoriasLivro = [
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Romance"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Computação"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Letras"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Gestão"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Administração"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Conto"),
+    new CategoriaLivroEntity_1.CategoriaLivroEntity(undefined, "Ficção Científica"),
+];
---
+++ src/repository/CategoriaLivroRepository.ts
@@ -0,0 +1,11 @@
+import { CategoriaLivroEntity } from "../model/CategoriaLivroEntity";
+
+export const categoriasLivro: CategoriaLivroEntity[] = [
+  new CategoriaLivroEntity(undefined, "Romance"),
+  new CategoriaLivroEntity(undefined, "Computação"),
+  new CategoriaLivroEntity(undefined, "Letras"),
+  new CategoriaLivroEntity(undefined, "Gestão"),
+  new CategoriaLivroEntity(undefined, "Administração"),
+  new CategoriaLivroEntity(undefined, "Conto"),
+  new CategoriaLivroEntity(undefined, "Ficção Científica"),
+]
