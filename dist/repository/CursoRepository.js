a9d201540df41cf53575a21f7e7a6006f1a68be6
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-03 17:59:04 -0300

    Criação do repository 'CursoRepository'

---
+++ dist/repository/CursoRepository.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cursos = void 0;
+const CursoEntity_1 = require("../model/CursoEntity");
+exports.cursos = [
+    new CursoEntity_1.CursoEntity(undefined, "Adiministração"),
+    new CursoEntity_1.CursoEntity(undefined, "Análise e Desenvolvimento de Sistemas"),
+    new CursoEntity_1.CursoEntity(undefined, "Educação Física"),
+    new CursoEntity_1.CursoEntity(undefined, "Engenharia da Computação"),
+    new CursoEntity_1.CursoEntity(undefined, "Nutrição"),
+    new CursoEntity_1.CursoEntity(undefined, "Pedagogia"),
+    new CursoEntity_1.CursoEntity(undefined, "Odontologia"),
+    new CursoEntity_1.CursoEntity(undefined, "Psicologia"),
+    new CursoEntity_1.CursoEntity(undefined, "Farmácia"),
+    new CursoEntity_1.CursoEntity(undefined, "Direito"),
+];
---
+++ src/repository/CursoRepository.ts
@@ -0,0 +1,14 @@
+import { CursoEntity } from "../model/CursoEntity";
+
+export const cursos: CursoEntity[] = [
+  new CursoEntity(undefined, "Adiministração"),
+  new CursoEntity(undefined, "Análise e Desenvolvimento de Sistemas"),
+  new CursoEntity(undefined, "Educação Física"),
+  new CursoEntity(undefined, "Engenharia da Computação"),
+  new CursoEntity(undefined, "Nutrição"),
+  new CursoEntity(undefined, "Pedagogia"),
+  new CursoEntity(undefined, "Odontologia"),
+  new CursoEntity(undefined, "Psicologia"),
+  new CursoEntity(undefined, "Farmácia"),
+  new CursoEntity(undefined, "Direito"),
+]
