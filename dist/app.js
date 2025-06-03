7f002a5afda2b05ed75e988e96c335bd0e85fbee
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-03 18:24:56 -0300

    Criação de endpoints de catálogos(categorias-usuario, categorias-livro, cursos)

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const CatalogoController_1 = require("./controller/CatalogoController");
+const catalogoController = new CatalogoController_1.CatalogoController();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = process.env.PORT ?? 3090;
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Deu certo" });
-});
+app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
+app.get("/library/catalogos/categorias-livro", catalogoController.listarCategoriasLivro.bind(catalogoController));
+app.get("/library/catalogos/cursos", catalogoController.listarCursos.bind(catalogoController));
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import { CatalogoController } from "./controller/CatalogoController";
+
+const catalogoController = new CatalogoController()
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT ?? 3090;
+
+app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
+
+app.get("/library/catalogos/categorias-livro", catalogoController.listarCategoriasLivro.bind(catalogoController));
+
+app.get("/library/catalogos/cursos", catalogoController.listarCursos.bind(catalogoController));
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
