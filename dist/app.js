b5204461fb00ccda28e9fca967ad4df4e24e1bf8
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-05 18:07:24 -0300

    Criação de endpoints de livros

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const CatalogoController_1 = require("./controller/CatalogoController");
 const UsuarioController_1 = require("./controller/UsuarioController");
+const LivroController_1 = require("./controller/LivroController");
 const usuarioController = new UsuarioController_1.UsuarioController();
+const livroController = new LivroController_1.LivroController();
 const catalogoController = new CatalogoController_1.CatalogoController();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -17,6 +19,12 @@ app.get("/library/usuarios/:cpf", usuarioController.listarUsuarioPorCpf.bind(usu
 app.post("/library/usuarios", usuarioController.cadastrarUsuario.bind(usuarioController));
 app.put("/library/usuarios/:cpf", usuarioController.atualizarUsuario.bind(usuarioController));
 app.delete("/library/usuarios/:cpf", usuarioController.removerUsuario.bind(usuarioController));
+//Livros
+app.get("/library/livros", livroController.listarLivros.bind(livroController));
+app.get("/library/livros/:isbn", livroController.listarLivroPorIsbn.bind(livroController));
+app.post("/library/livros", livroController.cadastrarLivro.bind(livroController));
+app.put("/library/livros/:isbn", livroController.atualizarLivro.bind(livroController));
+app.delete("/library/livros/:isbn", livroController.removerLivro.bind(livroController));
 //Catalogos
 app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
 app.get("/library/catalogos/categorias-livro", catalogoController.listarCategoriasLivro.bind(catalogoController));
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { CatalogoController } from "./controller/CatalogoController";
 import { UsuarioController } from "./controller/UsuarioController";
+import { LivroController } from "./controller/LivroController";
 
 const usuarioController = new UsuarioController();
+const livroController = new LivroController();
 const catalogoController = new CatalogoController();
 
 const app = express();
@@ -16,6 +18,13 @@ app.get("/library/usuarios/:cpf", usuarioController.listarUsuarioPorCpf.bind(usu
 app.post("/library/usuarios", usuarioController.cadastrarUsuario.bind(usuarioController));
 app.put("/library/usuarios/:cpf", usuarioController.atualizarUsuario.bind(usuarioController));
 app.delete("/library/usuarios/:cpf", usuarioController.removerUsuario.bind(usuarioController));
+
+//Livros
+app.get("/library/livros", livroController.listarLivros.bind(livroController));
+app.get("/library/livros/:isbn", livroController.listarLivroPorIsbn.bind(livroController));
+app.post("/library/livros", livroController.cadastrarLivro.bind(livroController));
+app.put("/library/livros/:isbn", livroController.atualizarLivro.bind(livroController));
+app.delete("/library/livros/:isbn", livroController.removerLivro.bind(livroController));
 
 //Catalogos
 app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
