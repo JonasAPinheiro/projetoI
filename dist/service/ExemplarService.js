1c07f88c9ce6f0c880a59d1382c3e3782ac4fe4b
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-05 19:15:38 -0300

    Criação do service 'ExemplarService'

---
+++ dist/service/ExemplarService.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ExemplarService = void 0;
+const ExemplarEntity_1 = require("../model/ExemplarEntity");
+const ExemplarRepository_1 = require("../repository/ExemplarRepository");
+class ExemplarService {
+    exemplarRepository = ExemplarRepository_1.ExemplarRepository.getInstance();
+    exibeExemplares() {
+        return this.exemplarRepository.exibirExemplares();
+    }
+    exibeExemplarPorCodigo(codigo) {
+        return this.exemplarRepository.exibirExemplarPorCodigo(codigo);
+    }
+    novoExemplar(data) {
+        if (!data.quantidade || !data.quantidadeEmprestada || !data.disponivel || !data.livroId) {
+            throw new Error("Preencha todos os campos !!!");
+        }
+        const exemplar = new ExemplarEntity_1.ExemplarEntity(undefined, data.quantidade, data.quantidadeEmprestada, data.disponivel, data.livroId);
+        this.exemplarRepository.insereExemplar(exemplar);
+        return exemplar;
+    }
+    atualizaExemplar(codigo, data) {
+        const exemplarAtual = this.exemplarRepository.exibirExemplarPorCodigo(codigo);
+        if (!data.quantidade || !data.quantidadeEmprestada || !data.disponivel || !data.livroId) {
+            throw new Error("Preencha todos os campos !!!");
+        }
+        const novoExemplar = new ExemplarEntity_1.ExemplarEntity(exemplarAtual.codigo, data.quantidade, data.quantidadeEmprestada, data.disponivel, data.livroId);
+        this.exemplarRepository.atualizaExemplar(codigo, novoExemplar);
+        return novoExemplar;
+    }
+    removeExemplar(codigo) {
+        this.exemplarRepository.removeExemplar(codigo);
+    }
+}
+exports.ExemplarService = ExemplarService;
---
+++ src/service/ExemplarService.ts
@@ -0,0 +1,41 @@
+import { ExemplarEntity } from "../model/ExemplarEntity";
+import { ExemplarRepository } from "../repository/ExemplarRepository";
+
+export class ExemplarService {
+  private exemplarRepository = ExemplarRepository.getInstance();
+
+  exibeExemplares(): ExemplarEntity[] {
+    return this.exemplarRepository.exibirExemplares();
+  }
+
+  exibeExemplarPorCodigo(codigo: number): ExemplarEntity{
+    return this.exemplarRepository.exibirExemplarPorCodigo(codigo);
+  }
+
+  novoExemplar(data: any): ExemplarEntity {
+    if(!data.quantidade || !data.quantidadeEmprestada || !data.disponivel || !data.livroId) {
+      throw new Error("Preencha todos os campos !!!");
+    }
+    const exemplar = new ExemplarEntity(undefined, data.quantidade, data.quantidadeEmprestada, data.disponivel, data.livroId);
+
+    this.exemplarRepository.insereExemplar(exemplar);
+    return exemplar;
+  }
+
+  atualizaExemplar(codigo: number, data: any): ExemplarEntity {
+    const exemplarAtual = this.exemplarRepository.exibirExemplarPorCodigo(codigo);
+    if(!data.quantidade || !data.quantidadeEmprestada || !data.disponivel || !data.livroId) {
+      throw new Error("Preencha todos os campos !!!");
+    }
+
+    const novoExemplar = new ExemplarEntity(exemplarAtual.codigo, data.quantidade, data.quantidadeEmprestada, data.disponivel, data.livroId);
+
+    this.exemplarRepository.atualizaExemplar(codigo, novoExemplar);
+
+    return novoExemplar
+  }
+
+  removeExemplar(codigo: number) {
+    this.exemplarRepository.removeExemplar(codigo);
+  }
+}
