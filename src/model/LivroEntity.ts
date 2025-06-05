234b44fb94e16999524de63cff1a907655360de0
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-05 18:05:53 -0300

    Criação da entity 'LivroEntity'

---
+++ src/model/LivroEntity.ts
@@ -0,0 +1,31 @@
+export class LivroEntity {
+  id: number;
+  titulo: string;
+  autor: string;
+  editora: string;
+  edicao: string;
+  isbn: string;
+  categoriaId: number;
+
+  constructor(
+    id: number | undefined,
+    titulo: string,
+    autor: string,
+    editora: string,
+    edicao: string,
+    isbn: string,
+    categoriaId: number
+  ) {
+    this.id = id ?? this.gerarId();
+    this.titulo = titulo;
+    this.autor = autor;
+    this.editora = editora;
+    this.edicao = edicao;
+    this.isbn = isbn;
+    this.categoriaId = categoriaId;
+  }
+
+  private gerarId(): number {
+    return parseInt((Date.now() / 100).toString(), 10);
+  }
+}
