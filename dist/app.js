fde041fe099997633a63d614f8c70c480cfa4eb6
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-06-05 15:18:53 -0300

    Criação de endpoints de usuários

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const CatalogoController_1 = require("./controller/CatalogoController");
+const UsuarioController_1 = require("./controller/UsuarioController");
+const usuarioController = new UsuarioController_1.UsuarioController();
 const catalogoController = new CatalogoController_1.CatalogoController();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = process.env.PORT ?? 3090;
+//Usuários
+app.get("/library/usuarios", usuarioController.listarUsuarios.bind(usuarioController));
+app.get("/library/usuarios/:cpf", usuarioController.listarUsuarioPorCpf.bind(usuarioController));
+app.post("/library/usuarios", usuarioController.cadastrarUsuario.bind(usuarioController));
+app.put("/library/usuarios/:cpf", usuarioController.atualizarUsuario.bind(usuarioController));
+app.delete("/library/usuarios/:cpf", usuarioController.removerUsuario.bind(usuarioController));
+//Catalogos
 app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
 app.get("/library/catalogos/categorias-livro", catalogoController.listarCategoriasLivro.bind(catalogoController));
 app.get("/library/catalogos/cursos", catalogoController.listarCursos.bind(catalogoController));
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import express from "express";
 import { CatalogoController } from "./controller/CatalogoController";
+import { UsuarioController } from "./controller/UsuarioController";
 
-const catalogoController = new CatalogoController()
+const usuarioController = new UsuarioController();
+const catalogoController = new CatalogoController();
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT ?? 3090;
 
+//Usuários
+app.get("/library/usuarios", usuarioController.listarUsuarios.bind(usuarioController));
+app.get("/library/usuarios/:cpf", usuarioController.listarUsuarioPorCpf.bind(usuarioController));
+app.post("/library/usuarios", usuarioController.cadastrarUsuario.bind(usuarioController));
+app.put("/library/usuarios/:cpf", usuarioController.atualizarUsuario.bind(usuarioController));
+app.delete("/library/usuarios/:cpf", usuarioController.removerUsuario.bind(usuarioController));
+
+//Catalogos
 app.get("/library/catalogos/categorias-usuario", catalogoController.listarCategoriasUsuario.bind(catalogoController));
-
 app.get("/library/catalogos/categorias-livro", catalogoController.listarCategoriasLivro.bind(catalogoController));
-
 app.get("/library/catalogos/cursos", catalogoController.listarCursos.bind(catalogoController));
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
