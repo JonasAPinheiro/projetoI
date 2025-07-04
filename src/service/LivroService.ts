a7d286d9736e9dccedd65268dc399f84e195881f
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-04 18:47:18 -0300

    Modificando as validações nas classes 'UsuarioService', 'LivroService' e 'EmprestimoService' para funcionar na nova estrutura do catálogo

--- src/service/LivroService.ts
+++ src/service/LivroService.ts
@@ -1,9 +1,10 @@
 import { LivroEntity } from "../model/LivroEntity";
-import { categoriasLivro } from "../repository/CategoriaLivroRepository";
+import { CategoriaLivroRepository } from "../repository/CategoriaLivroRepository";
 import { LivroRepository } from "../repository/LivroRepository";
 
 export class LivroService {
   private livroRepository = LivroRepository.getInstance();
+  private categoriaLivroRepository = CategoriaLivroRepository.getInstance();
 
   exibeLivros(): LivroEntity[]{
     return this.livroRepository.exibirLivros();
@@ -13,13 +14,13 @@ export class LivroService {
     return this.livroRepository.exibirLivroPorIsbn(isbn);
   }
 
-  novoLivro(data: any): LivroEntity {
+  async novoLivro(data: any): Promise<LivroEntity> {
     if (!data.titulo || !data.autor || !data.editora || !data.edicao || !data.isbn || !data.categoriaId) {
       throw new Error("Preencha todos os campos!!!");
     }
 
-    this.validarCategoria(data.categoriaId);
-    this.validarCombinacao(data.autor, data.editora, data.edicao);
+    await this.validarCategoria(data.categoriaId);
+    await this.validarCombinacao(data.autor, data.editora, data.edicao);
 
     const livro = new LivroEntity(undefined, data.titulo, data.autor, data.editora, data.edicao, data.isbn, data.categoriaId);
 
@@ -27,13 +28,13 @@ export class LivroService {
     return livro;
   }
 
-  atualizaLivro(isbn: string, data: any): LivroEntity {
+  async atualizaLivro(isbn: string, data: any): Promise<LivroEntity> {
     const livroAtual = this.livroRepository.exibirLivroPorIsbn(isbn);
     if (!data.titulo || !data.autor || !data.editora || !data.edicao || !data.isbn || !data.categoriaId) {
       throw new Error("Preencha todos os campos!!!");
     }
 
-    this.validarCategoria(data.categoriaId);
+    await this.validarCategoria(data.categoriaId);
 
     const novoLivro = new LivroEntity(livroAtual.id, data.titulo, data.autor, data.editora, data.edicao, data.isbn, data.categoriaId);
 
@@ -46,14 +47,15 @@ export class LivroService {
     this.livroRepository.removeLivro(isbn);
   }
 
-  private validarCategoria(id: any): void {
-    if (!categoriasLivro.find(categoria => categoria.id === id)) 
-    {
+  private async validarCategoria(id: number): Promise<void> {
+    const categorias = await this.categoriaLivroRepository.exibirCategoriasLivros();
+    const existe = categorias.some((categoria) => categoria.id === id);
+    if (!existe) {
       throw new Error("Categoria não existe!!!");
     }
   }
 
-  private validarCombinacao(autor: string, editora: string, edicao: string): void {
+  private async validarCombinacao(autor: string, editora: string, edicao: string): Promise<void> {
     const livros = this.livroRepository.exibirLivros(); 
 
     const combinação = livros.find(livro => livro.autor == autor && livro.editora === editora &&livro.edicao == edicao);
