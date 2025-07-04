f839f992212b44b8c5bf4c8c6c491a1cfe15f4f6
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-04 18:32:24 -0300

    Atualização da 'CursoRepository' para integrar com um banco de dados

--- src/repository/CursoRepository.ts
+++ src/repository/CursoRepository.ts
@@ -1,14 +1,53 @@
+import executarComandoSQL from "../database/mysql";
 import { CursoEntity } from "../model/CursoEntity";
 
-export const cursos: CursoEntity[] = [
-  new CursoEntity(1, "Administração"),
-  new CursoEntity(2, "Análise e Desenvolvimento de Sistemas"),
-  new CursoEntity(3, "Educação Física"),
-  new CursoEntity(4, "Engenharia da Computação"),
-  new CursoEntity(5, "Nutrição"),
-  new CursoEntity(6, "Pedagogia"),
-  new CursoEntity(7, "Odontologia"),
-  new CursoEntity(8, "Psicologia"),
-  new CursoEntity(9, "Farmácia"),
-  new CursoEntity(10, "Direito"),
-]
+export class CursoRepository {
+  private static instance: CursoRepository;
+
+  private constructor() {
+    this.createTable();
+  }
+
+  static getInstance(): CursoRepository {
+    if (!this.instance) {
+      this.instance = new CursoRepository();
+    }
+    return this.instance;
+  }
+
+  private async createTable() {
+    const query = `CREATE TABLE IF NOT EXISTS projbiblioteca.Curso(
+      id int primary key auto_increment,
+      nome varchar(100) not null
+    )`;
+    try {
+      await executarComandoSQL(query, []);
+      console.log("Tabela Curso criada com sucesso!!!");
+      await this.insereCategoriasPadrao();
+    } catch (err) {
+      console.error(`Erro ao executar a query: ${err}`);
+    }
+  }
+
+  private async insereCategoriasPadrao() {
+    const resultado = await executarComandoSQL(`SELECT COUNT(*) as count FROM projbiblioteca.Curso`, []);
+
+    if (resultado[0].count > 0) {
+      return;
+    }
+
+    const cursos = ["Administração", "Análise e Desenvolvimento de Sistemas", "Educação Física", "Engenharia da Computação", "Nutrição"];
+
+    for (let i = 0; i < cursos.length; i++) {
+      await executarComandoSQL(`INSERT INTO projbiblioteca.Curso(nome) VALUES(?)`, [cursos[i]]);
+    }
+  }
+
+  async exibirCursos(): Promise<CursoEntity[]> {
+    const resultado = await executarComandoSQL(`SELECT * FROM projbiblioteca.Curso`, []);
+
+    return resultado.map((linha: any) => {
+      return new CursoEntity(linha.id, linha.nome);
+    });
+  }
+}
