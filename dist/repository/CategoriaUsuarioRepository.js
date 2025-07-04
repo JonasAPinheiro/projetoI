2cd4f2be6e46e5de2ffb5530554930eb7a9f9da1
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-04 10:25:25 -0300

    Removendo dist do controle do Git no arquivo '.gitignore'

--- dist/repository/CategoriaUsuarioRepository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categoriasUsuario = void 0;
-const CategoriaUsuarioEntity_1 = require("../model/CategoriaUsuarioEntity");
-exports.categoriasUsuario = [
-    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(1, "Professor"),
-    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(2, "Aluno"),
-    new CategoriaUsuarioEntity_1.CategoriaUsuarioEntity(3, "Bibliotecário"),
-];
