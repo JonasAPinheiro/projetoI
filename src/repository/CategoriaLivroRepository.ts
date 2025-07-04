629a68be358e98b45d130e2ad5e8dc609510c049
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-04 18:32:08 -0300

    Atualização da 'CategoriaLivroRepository' para integrar com um banco de dados

--- src/repository/CategoriaLivroRepository.ts
+++ src/repository/CategoriaLivroRepository.ts
@@ -1,11 +1,53 @@
+import executarComandoSQL from "../database/mysql";
 import { CategoriaLivroEntity } from "../model/CategoriaLivroEntity";
 
-export const categoriasLivro: CategoriaLivroEntity[] = [
-  new CategoriaLivroEntity(1, "Romance"),
-  new CategoriaLivroEntity(2, "Computação"),
-  new CategoriaLivroEntity(3, "Letras"),
-  new CategoriaLivroEntity(4, "Gestão"),
-  new CategoriaLivroEntity(5, "Administração"),
-  new CategoriaLivroEntity(6, "Conto"),
-  new CategoriaLivroEntity(7, "Ficção Científica"),
-];
+export class CategoriaLivroRepository {
+  private static instance: CategoriaLivroRepository;
+
+  private constructor() {
+    this.createTable();
+  }
+
+  static getInstance(): CategoriaLivroRepository {
+    if (!this.instance) {
+      this.instance = new CategoriaLivroRepository();
+    }
+    return this.instance;
+  }
+
+  private async createTable() {
+    const query = `CREATE TABLE IF NOT EXISTS projbiblioteca.CategoriaLivro(
+      id int primary key auto_increment,
+      nome varchar(100) not null
+    )`;
+    try {
+      await executarComandoSQL(query, []);
+      console.log("Tabela CategoriaLivro criada com sucesso!!!");
+      await this.insereCategoriasPadrao();
+    } catch (err) {
+      console.error(`Erro ao executar a query: ${err}`);
+    }
+  }
+
+  private async insereCategoriasPadrao() {
+    const resultado = await executarComandoSQL(`SELECT COUNT(*) as count FROM projbiblioteca.CategoriaLivro`, []);
+
+    if (resultado[0].count > 0) {
+      return;
+    }
+
+    const categorias = ["Romance", "Computação", "Letras", "Gestão", "Administração", "Conto", "Ficção Científica"];
+
+    for (let i = 0; i < categorias.length; i++) {
+      await executarComandoSQL(`INSERT INTO projbiblioteca.CategoriaLivro(nome) VALUES(?)`, [categorias[i]]);
+    }
+  }
+
+  async exibirCategoriasLivros(): Promise<CategoriaLivroEntity[]> {
+    const resultado = await executarComandoSQL(`SELECT * FROM projbiblioteca.CategoriaLivro`, []);
+
+    return resultado.map((linha: any) => {
+      return new CategoriaLivroEntity(linha.id, linha.nome);
+    });
+  }
+}
