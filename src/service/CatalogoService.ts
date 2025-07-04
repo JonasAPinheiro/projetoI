dc900995e414ce979e6f448b803c2f86cf625276
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-04 18:41:14 -0300

    Atualização da 'CatalogoService' para integrar com um banco de dados

--- src/service/CatalogoService.ts
+++ src/service/CatalogoService.ts
@@ -1,17 +1,24 @@
-import { categoriasLivro } from "../repository/CategoriaLivroRepository";
-import { categoriasUsuario } from "../repository/CategoriaUsuarioRepository";
-import { cursos } from "../repository/CursoRepository";
+import { CategoriaLivroEntity } from "../model/CategoriaLivroEntity";
+import { CategoriaUsuarioEntity } from "../model/CategoriaUsuarioEntity";
+import { CursoEntity } from "../model/CursoEntity";
+import { CategoriaLivroRepository } from "../repository/CategoriaLivroRepository";
+import { CategoriaUsuarioRepository } from "../repository/CategoriaUsuarioRepository";
+import { CursoRepository } from "../repository/CursoRepository";
 
-export class CatalogoService{
-  listarCategoriasUsuarios(){
-    return categoriasUsuario;
+export class CatalogoService {
+  private categoriaUsuarioRepository = CategoriaUsuarioRepository.getInstance();
+  private categoriaLivroRepository = CategoriaLivroRepository.getInstance();
+  private cursoRepository = CursoRepository.getInstance();
+
+  async listarCategoriasUsuarios(): Promise<CategoriaUsuarioEntity[]> {
+    return await this.categoriaUsuarioRepository.exibirCategoriasUsuarios();
   }
 
-  listarCategoriasLivros(){
-    return categoriasLivro;
+  async listarCategoriasLivros(): Promise<CategoriaLivroEntity[]> {
+    return await this.categoriaLivroRepository.exibirCategoriasLivros();
   }
 
-  listarCursos(){
-    return cursos;
+  async listarCursos(): Promise<CursoEntity[]> {
+    return await this.cursoRepository.exibirCursos();
   }
 }
