722a5605add58c2dc67e046d1f70b6d0067e529c
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-08 17:11:02 -0300

    Atualização da 'ExemplarEntity' para integrar com um banco de dados

--- src/model/ExemplarEntity.ts
+++ src/model/ExemplarEntity.ts
@@ -1,4 +1,5 @@
 export class ExemplarEntity {
+  id: number;
   codigo: number;
   quantidade: number;
   quantidadeEmprestada: number;
@@ -6,6 +7,7 @@ export class ExemplarEntity {
   livroId: number;
 
   constructor(
+    id: number | undefined,
     codigo: number | undefined,
     quantidade: number,
     quantidadeEmprestada: number,
@@ -15,6 +17,7 @@ export class ExemplarEntity {
       throw new Error("Quantidade emprestada maior que quantidade no estoque!!!");
     }
 
+    this.id = id ?? 0;
     this.codigo = codigo ?? this.gerarCodigo();
     this.quantidade = quantidade;
     this.quantidadeEmprestada = quantidadeEmprestada;
