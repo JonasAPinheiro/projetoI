53a4eccdfde48c70dcbd52f823022f7b4ab63420
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-08 17:28:11 -0300

    Atualização da 'ExemplarRepository' para integrar com um banco de dados

--- src/repository/ExemplarRepository.ts
+++ src/repository/ExemplarRepository.ts
@@ -1,10 +1,12 @@
+import executarComandoSQL from "../database/mysql";
 import { ExemplarEntity } from "../model/ExemplarEntity";
 
 export class ExemplarRepository {
   private static instance: ExemplarRepository;
-  private exemplarList: ExemplarEntity[] = [];
 
-  constructor() {}
+  constructor() {
+    this.createTable();
+  }
 
   static getInstance(): ExemplarRepository {
     if (!this.instance) {
@@ -13,34 +15,92 @@ export class ExemplarRepository {
     return this.instance;
   }
 
-  exibirExemplares(): ExemplarEntity[] {
-    return this.exemplarList;
-  }
+  private async createTable() {
+    const query = `CREATE TABLE IF NOT EXISTS projbiblioteca.Exemplar(
+      id int primary key auto_increment,
+      codigo bigint not null unique,
+      quantidade int not null,
+      quantidadeEmprestada int not null,
+      disponivel boolean not null, 
+      livroId int not null,
+      foreign key (livroId) references Livro(id)
+    )`;
 
-  exibirExemplarPorCodigo(codigo: number): ExemplarEntity {
-    const index = this.findIndex(codigo);
-    return this.exemplarList[index];
-  }
-
-  insereExemplar(exemplar: ExemplarEntity) {
-    this.exemplarList.push(exemplar);
-  }
-
-  atualizaExemplar(codigo: number, novoExemplar: ExemplarEntity) {
-    const index = this.findIndex(codigo);
-    this.exemplarList[index] = novoExemplar;
-  }
-
-  removeExemplar(codigo: number) {
-    const index = this.findIndex(codigo);
-    this.exemplarList.splice(index, 1);
-  }
-
-  private findIndex(codigo: number): number {
-    const index = this.exemplarList.findIndex((e) => e.codigo == codigo);
-    if (index == -1) {
-      throw new Error("O código do exemplar não foi encontrado !!!");
+    try {
+      const resultado = await executarComandoSQL(query, []);
+      console.log("Tabela Exemplar criada com sucesso!!!");
+    } catch (err) {
+      console.error("Erro ao executar a query: ", err);
     }
-    return index;
+  }
+
+  async exibirExemplares(): Promise<ExemplarEntity[]> {
+    const resultado = await executarComandoSQL(`SELECT * FROM projbiblioteca.Exemplar`,[]);
+
+    return resultado.map((linha: any) => {
+      return new ExemplarEntity(
+        linha.id, 
+        linha.codigo,
+        linha.quantidade,
+        linha.quantidadeEmprestada,
+        linha.livroId
+      );
+    });
+  }
+
+  async exibirExemplarPorCodigo(codigo: number): Promise<ExemplarEntity> {
+    const resultado = await executarComandoSQL(`SELECT * FROM projbiblioteca.Exemplar WHERE codigo = ?`, [codigo]);
+
+    const exemplar = resultado[0];
+
+    if(!exemplar) {
+      throw new Error("Exemplar não encontrado");
+    }
+
+    return new ExemplarEntity(
+      exemplar.id,
+      exemplar.codigo,
+      exemplar.quantidade,
+      exemplar.quantidadeEmprestada,
+      exemplar.livroId
+    )
+  }
+
+  async insereExemplar(exemplar: ExemplarEntity): Promise<ExemplarEntity> {
+    const { codigo, quantidade, quantidadeEmprestada, disponivel, livroId } = exemplar;
+
+    const resultado = await executarComandoSQL(
+      `
+      INSERT INTO projbiblioteca.Exemplar(codigo, quantidade, quantidadeEmprestada, disponivel, livroId) VALUES(?, ?, ?, ?, ?)
+      `,
+      [codigo, quantidade, quantidadeEmprestada, disponivel, livroId]
+    );
+
+    const novoExemplar = new ExemplarEntity(resultado.insertId, codigo, quantidade, quantidadeEmprestada, livroId);
+    console.log("Exemplar inserido com sucesso:", novoExemplar);
+    return novoExemplar;
+  }
+
+  async atualizaExemplar(codigo: number, novoExemplar: ExemplarEntity): Promise<ExemplarEntity> {
+    const { quantidade, quantidadeEmprestada, disponivel, livroId } = novoExemplar;
+
+    await executarComandoSQL(
+      `
+      UPDATE projbiblioteca.Exemplar SET quantidade = ?, quantidadeEmprestada = ?, disponivel = ?, livroId = ? WHERE codigo = ?
+      `,
+      [quantidade, quantidadeEmprestada, disponivel, livroId, codigo]
+    );
+
+    const exemplarAtualizado = await this.exibirExemplarPorCodigo(codigo);
+    console.log("Exemplar atualizado com sucesso:", exemplarAtualizado);
+    return exemplarAtualizado;
+  }
+
+  async removeExemplar(codigo: number): Promise<ExemplarEntity> {
+    const exemplarExcluido = await this.exibirExemplarPorCodigo(codigo);
+    await executarComandoSQL(`DELETE FROM projbiblioteca.Exemplar WHERE codigo = ?`, [codigo]);
+
+    console.log("Exemplar deletado com sucesso:", exemplarExcluido);
+    return exemplarExcluido;
   }
 }
