56684878baeba8750eac2cfca3808a7932d1bfc1
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-08 21:13:44 -0300

    Atualização da 'ExemplarController' para integrar com um banco de dados

--- src/controller/ExemplarController.ts
+++ src/controller/ExemplarController.ts
@@ -4,109 +4,110 @@ import { ExemplarService } from "../service/ExemplarService";
 export class ExemplarController {
   private exemplarService = new ExemplarService();
 
-  listarExemplares(req: Request, res: Response): void {
+  async listarExemplares(req: Request, res: Response): Promise<void> {
     try {
-      const exemplares = this.exemplarService.exibeExemplares();
+      const exemplares = await this.exemplarService.exibeExemplares();
       res.status(200).json(exemplares);
-    } catch (error: unknown) {
+    } catch (err: unknown) {
       let message: string = "Não foi possível listar os exemplares!!!";
-      if(error instanceof Error) {
-        message = error.message
+      if (err instanceof Error) {
+        message = err.message;
       }
       res.status(400).json({
-        message: message
+        message: message,
       });
     }
   }
 
-  listarExemplarPorCodigo(req: Request, res: Response): void {
+  async listarExemplarPorCodigo(req: Request, res: Response): Promise<void> {
     try {
-      const { codigo } = req.params
+      const { codigo } = req.params;
       const codigoNum = parseInt(codigo);
-      const exemplar = this.exemplarService.exibeExemplarPorCodigo(codigoNum);
-      
+      const exemplar = await this.exemplarService.exibeExemplarPorCodigo(codigoNum);
+
       if (isNaN(codigoNum)) {
         throw new Error("Código inválido!");
       }
-      
+
       res.status(200).json(exemplar);
-    } catch (error: unknown) {
+    } catch (err: unknown) {
       let message: string = "Não foi possível encontrar o exemplar com esse código!!!";
-      if(error instanceof Error) {
-        message = error.message
+      if (err instanceof Error) {
+        message = err.message;
       }
       res.status(400).json({
-        message: message
+        message: message,
       });
     }
   }
 
-  cadastrarExemplar(req: Request, res: Response): void {
+  async cadastrarExemplar(req: Request, res: Response): Promise<void> {
     try {
-      const exemplar = this.exemplarService.novoExemplar(req.body);
+      const exemplar = await this.exemplarService.novoExemplar(req.body);
 
       res.status(201).json({
         message: "Exemplar cadastrado com sucesso!!!",
         exemplar: exemplar,
       });
-    } catch (error: unknown) {
+    } catch (err: unknown) {
       let message: string = "Não foi possível cadastrar exemplar!!!";
-      if(error instanceof Error) {
-        message = error.message
+      if (err instanceof Error) {
+        message = err.message;
       }
       res.status(400).json({
-        message: message
+        message: message,
       });
     }
   }
 
-  atualizarExemplar(req: Request, res: Response): void {
+  async atualizarExemplar(req: Request, res: Response): Promise<void> {
     try {
-      const { codigo } = req.params
+      const { codigo } = req.params;
       const codigoNum = parseInt(codigo);
-      const exemplar = this.exemplarService.atualizaExemplar(codigoNum, req.body);
+      const exemplar = await this.exemplarService.atualizaExemplar(codigoNum, req.body);
 
       if (isNaN(codigoNum)) {
         throw new Error("Código inválido!");
       }
-      
+
       res.status(200).json({
         message: "Exemplar atualizado com sucesso!!!",
         exemplar: exemplar,
-      })
-    } catch (error: unknown) {
+      });
+    } catch (err: unknown) {
       let message: string = "Não foi possível ataualizar exemplar!!!";
-      if(error instanceof Error) {
-        message = error.message
+      if (err instanceof Error) {
+        message = err.message;
       }
       res.status(400).json({
-        message: message
+        message: message,
       });
     }
   }
 
-  removerExemplar(req: Request, res: Response): void {
+  async removerExemplar(req: Request, res: Response): Promise<void> {
     try {
-      const { codigo } = req.params
+      const { codigo } = req.params;
       const codigoNum = parseInt(codigo);
-      const exemplar = this.exemplarService.exibeExemplarPorCodigo(codigoNum);
-      
+
       if (isNaN(codigoNum)) {
         throw new Error("Código inválido!");
       }
-      
-      this.exemplarService.removeExemplar(codigoNum);
+
+      const exemplar = await this.exemplarService.exibeExemplarPorCodigo(codigoNum);
+
+      await this.exemplarService.removeExemplar(codigoNum);
       res.status(200).json({
         message: "Exemplar removido com sucesso!!!",
         exemplar: exemplar,
       });
-    } catch (error: unknown) {
+    } catch (err: unknown) {
       let message: string = "Não foi possível remover exemplar!!!";
-      if(error instanceof Error) {
-        message = error.message
+      if (err instanceof Error) {
+        message = err.message;
       }
       res.status(400).json({
-        message: message
+        message: message,
       });
     }
   }
