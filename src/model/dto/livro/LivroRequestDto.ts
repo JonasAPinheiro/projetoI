34aebb1b9e6a0c04f56f288f70032b372abbb8f6
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-10 16:19:37 -0300

    Crição dos DTOs 'LivroRequestDto' e 'LivroUpdateDto'

---
+++ src/model/dto/livro/LivroRequestDto.ts
@@ -0,0 +1,24 @@
+export class LivroRequestDto {
+  titulo: string;
+  autor: string;
+  editora: string;
+  edicao: string;
+  isbn: string;
+  categoriaId: number;
+
+  constructor(
+    titulo: string,
+    autor: string,
+    editora: string,
+    edicao: string,
+    isbn: string,
+    categoriaId: number
+  ) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.editora = editora;
+    this.edicao = edicao;
+    this.isbn = isbn;
+    this.categoriaId = categoriaId;
+  }
+}
