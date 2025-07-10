c8ff96480d129cbffcb0ac8536fb56ef887664ef
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-10 15:00:23 -0300

    Criação do DTO 'UsuarioRequestDto'

---
+++ src/model/dto/usuario/UsuarioRequestDto.ts
@@ -0,0 +1,26 @@
+type Status = "ativo" | "inativo" | "suspenso";
+
+export class UsuarioRequestDto {
+  nome: string;
+  email: string;
+  cpf: string;
+  ativo: Status;
+  categoriaId: number;
+  cursoId: number;
+
+  constructor(
+    nome: string,
+    email: string,
+    cpf: string,
+    ativo: Status,
+    categoriaId: number,
+    cursoId: number
+  ) {
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
+    this.ativo = ativo;
+    this.categoriaId = categoriaId;
+    this.cursoId = cursoId;
+  }
+}
