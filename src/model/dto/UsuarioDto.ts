300864c06747cc92a01c058aa7d05f088e0ffb13
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-07-10 14:34:16 -0300

    Criação do DTO 'UsuarioDto'

---
+++ src/model/dto/UsuarioDto.ts
@@ -0,0 +1,29 @@
+type Status = "ativo" | "inativo" | "suspenso";
+
+export class UsuarioDto {
+  id: number;
+  nome: string;
+  email: string;
+  cpf: string;
+  ativo: Status;
+  categoriaId: number;
+  cursoId: number;
+
+  constructor(
+    id: any,
+    nome: any,
+    email: any,
+    cpf: any,
+    ativo: any,
+    categoriaId: any,
+    cursoId: any
+  ) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
+    this.ativo = ativo;
+    this.categoriaId = categoriaId;
+    this.cursoId = cursoId;
+  }
+}
